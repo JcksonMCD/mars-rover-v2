d3f5da996ac0fcbca08d3650f2b1d68c2f08db7e
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-11-26 20:48:01 +0000

    Feat: parse instructions method created in instruction parser class.

--- src/main/java/org/northcoders/input/InstructionParser.java
+++ src/main/java/org/northcoders/input/InstructionParser.java
@@ -1,4 +1,11 @@
 package org.northcoders.input;
 
+import java.util.Arrays;
+import java.util.Queue;
+
 public class InstructionParser {
+
+    public Queue<Instruction> parseInstructions(String instructionsInput){
+
+    }
 }
