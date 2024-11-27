0de8d2ea7adf3f012af5b0cca2752481f36479cb
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-11-27 20:56:07 +0000

    Test: Tests added for turn righ method

--- src/test/java/org/northcoders/model/RoverTest.java
+++ src/test/java/org/northcoders/model/RoverTest.java
@@ -1,5 +1,6 @@
 package org.northcoders.model;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,7 +12,43 @@ class RoverTest {
     }
 
     @Test
-    void turnRight() {
+    @DisplayName("Turn right method alters facing field from N to E")
+    void turnRightNorthToEast() {
+        Rover rover = new Rover(new Position(5,5, CompassDirection.N));
+
+        rover.turnRight();
+
+        assertEquals(CompassDirection.E, rover.getPosition().getFacing());
+    }
+
+    @Test
+    @DisplayName("Turn right method alters facing field from E to S")
+    void turnRightEastToWest() {
+        Rover rover = new Rover(new Position(5,5, CompassDirection.E));
+
+        rover.turnRight();
+
+        assertEquals(CompassDirection.S, rover.getPosition().getFacing());
+    }
+
+    @Test
+    @DisplayName("Turn right method alters facing field from S to W")
+    void turnRightSouthToWest() {
+        Rover rover = new Rover(new Position(5,5, CompassDirection.S));
+
+        rover.turnRight();
+
+        assertEquals(CompassDirection.W, rover.getPosition().getFacing());
+    }
+
+    @Test
+    @DisplayName("Turn right method alters facing field from W to N")
+    void turnRightWestToNorth() {
+        Rover rover = new Rover(new Position(5,5, CompassDirection.W));
+
+        rover.turnRight();
+
+        assertEquals(CompassDirection.N, rover.getPosition().getFacing());
     }
 
     @Test
