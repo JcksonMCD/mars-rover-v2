0c714f34eee267c8762251b6307944c10ac7d3e5
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-11-27 15:53:14 +0000

    Test: Test created for direction parser- Return corrosponding enum type when passed a sing valid compass direction as a string.

--- src/test/java/org/northcoders/input/CompassDirectionParserTest.java
+++ src/test/java/org/northcoders/input/CompassDirectionParserTest.java
@@ -1,12 +1,23 @@
 package org.northcoders.input;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.northcoders.model.CompassDirection;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CompassDirectionParserTest {
 
     @Test
+    @DisplayName("Compass direction parser takes an input of a single compass direction and parses it to the enum type compass direction")
     void compassDirectionParser() {
+        CompassDirectionParser directionParser = new CompassDirectionParser();
+
+        assertAll(
+                () -> assertEquals(CompassDirection.N, directionParser.compassDirectionParser("N")),
+                () -> assertEquals(CompassDirection.N, directionParser.compassDirectionParser("E")),
+                () -> assertEquals(CompassDirection.N, directionParser.compassDirectionParser("S")),
+                () -> assertEquals(CompassDirection.N, directionParser.compassDirectionParser("W"))
+        );
     }
 }
