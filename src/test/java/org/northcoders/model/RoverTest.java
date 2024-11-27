3dd9228bc86deee5fb7dde4e76f525e827b6c200
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-11-27 20:47:38 +0000

    Test: Test class created for rover class.

---
+++ src/test/java/org/northcoders/model/RoverTest.java
@@ -0,0 +1,20 @@
+package org.northcoders.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoverTest {
+
+    @Test
+    void move() {
+    }
+
+    @Test
+    void turnRight() {
+    }
+
+    @Test
+    void turnLeft() {
+    }
+}
