9133f9fab68e55314dfed6e595b21f6f255b99b1
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-11-27 16:06:02 +0000

    Test: Simple test case added for when correct input is passed in to the method. Method should return a position instance with the values in the string.
    Test not passing- RED

--- src/test/java/org/northcoders/input/PositionParserTest.java
+++ src/test/java/org/northcoders/input/PositionParserTest.java
@@ -1,12 +1,20 @@
 package org.northcoders.input;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.northcoders.model.CompassDirection;
+import org.northcoders.model.Position;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PositionParserTest {
 
     @Test
+    @DisplayName("Parse starting position returns a position instance using the values given in the input string.")
     void parseStartingPosition() {
+        PositionParser positionParser = new PositionParser();
+        Position expectedOutput = new Position(5 , 5, CompassDirection.N);
+
+        assertEquals(expectedOutput, positionParser.parseStartingPosition("5 5 N"));
     }
 }
