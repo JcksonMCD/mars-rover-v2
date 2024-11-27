06557fe0c0c89e168d67deb3e6aeb3470cfb48aa
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-11-27 16:02:29 +0000

    Test: Test class created for position parser.

---
+++ src/test/java/org/northcoders/input/PositionParserTest.java
@@ -0,0 +1,12 @@
+package org.northcoders.input;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PositionParserTest {
+
+    @Test
+    void parseStartingPosition() {
+    }
+}
