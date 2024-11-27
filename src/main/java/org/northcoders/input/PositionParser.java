fa488a19c23c25d062599e0a89be3e103f606fc2
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-11-27 16:01:39 +0000

    Feat: Method signiture for parse starting position method added to position parser class.

--- src/main/java/org/northcoders/input/PositionParser.java
+++ src/main/java/org/northcoders/input/PositionParser.java
@@ -1,4 +1,11 @@
 package org.northcoders.input;
 
+import org.northcoders.model.Position;
+
 public class PositionParser {
+
+    public Position parseStartingPosition(String positionInput){
+
+        return null;
+    }
 }
