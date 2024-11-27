d65f219295ced725e47ac12745a7b4ff48ffe5ef
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-11-27 15:45:31 +0000

    Test: Test class created for compass direction parser.

---
+++ src/test/java/org/northcoders/input/CompassDirectionParserTest.java
@@ -0,0 +1,12 @@
+package org.northcoders.input;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompassDirectionParserTest {
+
+    @Test
+    void compassDirectionParser() {
+    }
+}
