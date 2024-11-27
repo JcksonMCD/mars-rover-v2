00bfd9851a31abaa7ca7d7fdf5f52d548cb8fa87
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-11-27 16:36:18 +0000

    Test: Parse plateau test class created.

---
+++ src/test/java/org/northcoders/input/PlateauParserTest.java
@@ -0,0 +1,12 @@
+package org.northcoders.input;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlateauParserTest {
+
+    @Test
+    void parsePlateauInput() {
+    }
+}
