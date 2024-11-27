525cebd245b87878ea6693fc8f5bea041ccd8492
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-11-27 16:11:05 +0000

    Feat: method body for parse starting position added.
    Test: All tests now parse for parse starting positon.

--- src/main/java/org/northcoders/input/PositionParser.java
+++ src/main/java/org/northcoders/input/PositionParser.java
@@ -1,11 +1,17 @@
 package org.northcoders.input;
 
+import org.northcoders.model.CompassDirection;
 import org.northcoders.model.Position;
 
 public class PositionParser {
 
     public Position parseStartingPosition(String positionInput){
+        String[] splitPositionValues = positionInput.split(" ");
 
-        return null;
+        int x = Integer.parseInt(splitPositionValues[0]);
+        int y = Integer.parseInt(splitPositionValues[1]);
+        CompassDirection facing = CompassDirection.valueOf(splitPositionValues[2]);
+
+        return new Position(x, y, facing);
     }
 }
