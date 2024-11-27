9d133bcb904cabd6b40930880c685e3ba3109ed0
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-11-27 19:50:36 +0000

    Feat: Rover class edited to implement drivable interface.

--- src/main/java/org/northcoders/model/Rover.java
+++ src/main/java/org/northcoders/model/Rover.java
@@ -1,4 +1,18 @@
 package org.northcoders.model;
 
-public class Rover {
+public class Rover implements Driveable{
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void turnRight() {
+
+    }
+
+    @Override
+    public void turnLeft() {
+
+    }
 }
