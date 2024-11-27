27345d0b12d7d2637b2cf0f2b317f3275c7d73af
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-11-27 16:39:39 +0000

    Test: Parse plateau test created- when provided input of two ints seperated by a space a plateau of that size should be returned.

--- src/test/java/org/northcoders/input/PlateauParserTest.java
+++ src/test/java/org/northcoders/input/PlateauParserTest.java
@@ -1,12 +1,20 @@
 package org.northcoders.input;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.northcoders.model.Plateau;
+import org.northcoders.model.Position;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlateauParserTest {
 
     @Test
+    @DisplayName("When provided with two space separated ints a plateau of that size is returned.")
     void parsePlateauInput() {
+        PlateauParser plateauParser = new PlateauParser();
+        Plateau expectedOutput = new Plateau(5, 5);
+
+        assertEquals(expectedOutput, plateauParser.parsePlateauInput("5 5"));
     }
 }
