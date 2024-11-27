05506d3fe8c99136cc3c79f7f7fe51d5c91d00dd
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-11-27 19:04:37 +0000

    Refactor: Whitespace removal logic refactored out into helper method in position parser class.

--- src/main/java/org/northcoders/input/PositionParser.java
+++ src/main/java/org/northcoders/input/PositionParser.java
@@ -10,19 +10,24 @@ import java.util.Objects;
 
 public class PositionParser {
 
-    public boolean isValidPositionFormat(String positionInput){
+    private boolean isValidPositionFormat(String positionInput){
         return positionInput.matches("^\\d+\\s+\\d+\\s+[neswNESW]$");
+    }
+
+    private ArrayList<String> removeWhitespaceFromArray(String[] arrayWithWhitespace){
+        ArrayList<String> valuesMinusWhitespace = new ArrayList<>();
+
+        for(String value : arrayWithWhitespace){
+            if (!value.equals(" ") && !value.isEmpty()) valuesMinusWhitespace.add(value);
+        }
+
+        return  valuesMinusWhitespace;
     }
 
     public Position parseStartingPosition(String positionInput){
         if (!isValidPositionFormat(positionInput.strip())) throw new IllegalArgumentException("Invalid format: You must use X Y N/E/S/W");
 
-        String[] splitPositionValues = positionInput.split(" ");
-        ArrayList<String> positionValuesMinusWhitespace = new ArrayList<>();
-
-        for(String value : splitPositionValues){
-            if (!value.equals(" ") && !value.isEmpty()) positionValuesMinusWhitespace.add(value);
-        }
+        ArrayList<String> positionValuesMinusWhitespace = removeWhitespaceFromArray(positionInput.split(" "));
 
         int x = Integer.parseInt(positionValuesMinusWhitespace.getFirst());
         int y = Integer.parseInt(positionValuesMinusWhitespace.get(1));
