350f6970ba8c76ba8daada0c649ef5f749dee7ab
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-11-27 19:12:25 +0000

    Refactor: Position parser refactored to provide a simpler solution to ignoring inconsistent whitespaces.
    Feat: Platea parser method altered to ignore inconsistent whitespace.

--- src/main/java/org/northcoders/input/PlateauParser.java
+++ src/main/java/org/northcoders/input/PlateauParser.java
@@ -1,12 +1,12 @@
 package org.northcoders.input;
 
 import org.northcoders.model.Plateau;
-import org.northcoders.model.Position;
+
 
 public class PlateauParser {
 
     public Plateau parsePlateauInput(String plateauInput){
-        String[] seperatedXandY = plateauInput.split(" ");
+        String[] seperatedXandY = plateauInput.split("\s+");
 
         int x = Integer.parseInt(seperatedXandY[0]);
         int y = Integer.parseInt(seperatedXandY[1]);
--- src/main/java/org/northcoders/input/PositionParser.java
+++ src/main/java/org/northcoders/input/PositionParser.java
@@ -3,10 +3,6 @@ package org.northcoders.input;
 import org.northcoders.model.CompassDirection;
 import org.northcoders.model.Position;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
 
 public class PositionParser {
 
@@ -14,24 +10,14 @@ public class PositionParser {
         return positionInput.matches("^\\d+\\s+\\d+\\s+[neswNESW]$");
     }
 
-    private ArrayList<String> removeWhitespaceFromArray(String[] arrayWithWhitespace){
-        ArrayList<String> valuesMinusWhitespace = new ArrayList<>();
-
-        for(String value : arrayWithWhitespace){
-            if (!value.equals(" ") && !value.isEmpty()) valuesMinusWhitespace.add(value);
-        }
-
-        return  valuesMinusWhitespace;
-    }
-
     public Position parseStartingPosition(String positionInput){
         if (!isValidPositionFormat(positionInput.strip())) throw new IllegalArgumentException("Invalid format: You must use X Y N/E/S/W");
 
-        ArrayList<String> positionValuesMinusWhitespace = removeWhitespaceFromArray(positionInput.split(" "));
+        String[] positionValuesMinusWhitespace = positionInput.strip().split("\s+");
 
-        int x = Integer.parseInt(positionValuesMinusWhitespace.getFirst());
-        int y = Integer.parseInt(positionValuesMinusWhitespace.get(1));
-        CompassDirection facing = CompassDirection.valueOf(positionValuesMinusWhitespace.get(2).toUpperCase());
+        int x = Integer.parseInt(positionValuesMinusWhitespace[0]);
+        int y = Integer.parseInt(positionValuesMinusWhitespace[1]);
+        CompassDirection facing = CompassDirection.valueOf(positionValuesMinusWhitespace[2].toUpperCase());
 
         return new Position(x, y, facing);
     }
