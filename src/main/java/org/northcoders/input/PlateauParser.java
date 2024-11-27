00578f666db544bb14cb69079f5b76d4f1ac6eb2
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-11-27 16:42:36 +0000

    Feat: Method body added to parse plateau input.
    Test now passes- GREEN.

--- src/main/java/org/northcoders/input/PlateauParser.java
+++ src/main/java/org/northcoders/input/PlateauParser.java
@@ -1,11 +1,16 @@
 package org.northcoders.input;
 
 import org.northcoders.model.Plateau;
+import org.northcoders.model.Position;
 
 public class PlateauParser {
 
     public Plateau parsePlateauInput(String plateauInput){
+        String[] seperatedXandY = plateauInput.split(" ");
 
-        return null;
+        int x = Integer.parseInt(seperatedXandY[0]);
+        int y = Integer.parseInt(seperatedXandY[1]);
+
+        return new Plateau(x, y);
     }
 }
