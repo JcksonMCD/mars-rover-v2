e384a56242f90143a5ffb68a31f10d2fc1e8697f
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-11-27 15:44:47 +0000

    Feat: Parser method added to compass direction parser.

--- src/main/java/org/northcoders/input/CompassDirectionParser.java
+++ src/main/java/org/northcoders/input/CompassDirectionParser.java
@@ -1,4 +1,10 @@
 package org.northcoders.input;
 
+import org.northcoders.model.CompassDirection;
+
 public class CompassDirectionParser {
+
+    public CompassDirection compassDirectionParser(String directionString){
+        return null;
+    }
 }
