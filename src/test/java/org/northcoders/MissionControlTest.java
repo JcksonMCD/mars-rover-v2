5c6ee28583270b25396af50c88e67fc821d43806
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-11-28 19:57:41 +0000

    Test: is position free return true when position is free.
    Test not passing: RED.

--- src/test/java/org/northcoders/MissionControlTest.java
+++ src/test/java/org/northcoders/MissionControlTest.java
@@ -1,12 +1,26 @@
 package org.northcoders;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.northcoders.model.Plateau;
+import org.northcoders.model.Position;
+import org.northcoders.model.Rover;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.northcoders.model.CompassDirection.N;
 
 class MissionControlTest {
 
     @Test
-    void isPositionFree() {
+    @DisplayName("Is position free returns true when position is free")
+    void isPositionFreeReturnsTrueWhenPositionIsFree() {
+        ArrayList<Rover> rovers = new ArrayList<>();
+        rovers.add(new Rover(new Position(0, 0, N)));
+        MissionControl missionControl = new MissionControl(new Plateau(1, 1), rovers);
+
+        assertTrue(missionControl.isPositionFree(new Position(1, 1, N)));
     }
 }
