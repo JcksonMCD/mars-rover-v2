98a2f910e3a9ae927f07ddc09d25a273607ed294
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-11-28 19:43:32 +0000

    Feat: Rover list and plateau fields added to Mission control class.

--- src/main/java/org/northcoders/MissionControl.java
+++ src/main/java/org/northcoders/MissionControl.java
@@ -1,4 +1,11 @@
 package org.northcoders;
 
+import org.northcoders.model.Plateau;
+import org.northcoders.model.Rover;
+
+import java.util.ArrayList;
+
 public class MissionControl {
+    private Plateau plateau;
+    private ArrayList<Rover> rovers;
 }
