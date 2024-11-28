f570b66a50d8514d382c889cce3dfa29a0d9ed65
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-11-28 19:44:58 +0000

    Feat: All args constructor, getters and setters added to Mission Control class.

--- src/main/java/org/northcoders/MissionControl.java
+++ src/main/java/org/northcoders/MissionControl.java
@@ -8,4 +8,25 @@ import java.util.ArrayList;
 public class MissionControl {
     private Plateau plateau;
     private ArrayList<Rover> rovers;
+
+    public MissionControl(Plateau plateau, ArrayList<Rover> rovers) {
+        this.plateau = plateau;
+        this.rovers = rovers;
+    }
+
+    public Plateau getPlateau() {
+        return plateau;
+    }
+
+    public void setPlateau(Plateau plateau) {
+        this.plateau = plateau;
+    }
+
+    public ArrayList<Rover> getRovers() {
+        return rovers;
+    }
+
+    public void setRovers(ArrayList<Rover> rovers) {
+        this.rovers = rovers;
+    }
 }
