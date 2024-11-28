b9fef39f21ed651862770c76fa0a50dd036d5d34
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-11-28 19:30:03 +0000

    Feat: Values added to compass enum class.

--- src/main/java/org/northcoders/model/CompassDirection.java
+++ src/main/java/org/northcoders/model/CompassDirection.java
@@ -1,8 +1,16 @@
 package org.northcoders.model;
 
 public enum CompassDirection {
-    N,
-    E,
-    S,
-    W
+    N(0, 1),
+    E(1, 0),
+    S(0, -1),
+    W(-1, 0);
+
+    public final int incrementX;
+    public final int incrementY;
+
+    CompassDirection(int incrementX, int incrementY) {
+        this.incrementX = incrementX;
+        this.incrementY = incrementY;
+    }
 }
