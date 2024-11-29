fccb64c1e09b2501558e4d39cb1e38d7c795fec6
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-11-29 18:13:25 +0000

    Feat: Constructor and fields added to UI class

--- src/main/java/org/northcoders/ui/UI.java
+++ src/main/java/org/northcoders/ui/UI.java
@@ -1,4 +1,16 @@
 package org.northcoders.ui;
 
+import org.northcoders.MissionControl;
+
+import java.util.Scanner;
+
 public class UI {
+    private State state;
+    private Scanner scanner;
+    private MissionControl missionControl;
+
+    public UI() {
+        state = State.PLATEAU_SETUP;
+        scanner = new Scanner(System.in);
+    }
 }
