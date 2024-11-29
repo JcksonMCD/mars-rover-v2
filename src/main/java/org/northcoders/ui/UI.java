a0ee267ab10e7a896e1c87d0075304978d1e1639
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-11-29 18:19:17 +0000

    Feat: Welcome method added to UI class.

--- src/main/java/org/northcoders/ui/UI.java
+++ src/main/java/org/northcoders/ui/UI.java
@@ -10,9 +10,18 @@ public class UI {
     private MissionControl missionControl;
 
     public UI() {
-        state = State.PLATEAU_SETUP;
+        state = State.WELCOME;
         scanner = new Scanner(System.in);
     }
+
+    public State welcome(){
+        System.out.println("WELCOME TO MARS: ");
+        System.out.println("It's your first day at Mission Control so you have been given access to a top of the range deploy rover simulator!");
+        System.out.println("Start by entering the size of the plateau you wish to work with and then land your first rover...");
+
+        return State.PLATEAU_SETUP;
+    }
+
 
 
 }
